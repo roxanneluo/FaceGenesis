c5ac1c24150b0570e36c8c52d437833d5961e65c
CHUNG-YI WENG <chungyi@cs.washington.edu>
2016-05-25 16:50:51 -0700

    complete face detection

--- main.cpp
+++ main.cpp
@@ -32,6 +32,26 @@ int error_happened(const ErrorType error_type)
     return -1;
 }
 
+void draw_faces(const Mat &image, vector<Rect> &faces)
+{
+    Scalar red_color = Scalar(0, 0, 255);
+    for (size_t i = 0; i < faces.size(); i++)
+    {
+        Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
+        ellipse(image, center, 
+                Size(faces[i].width / 2, faces[i].height / 2),
+                0, 0, 360, red_color, 4);
+    }
+}
+
+void detect_face(const Mat &gray_image, vector<Rect> &faces)
+{
+    faces.clear();
+
+    equalizeHist(gray_image, gray_image);
+    face_cascade.detectMultiScale(gray_image, faces, 1.21, 3, 0, Size(30, 30));
+}
+
 int main(int, char**)
 {
     if (!face_cascade.load(face_cascade_name))
@@ -50,6 +70,10 @@ int main(int, char**)
 
         Mat gray_image;
         cvtColor(image, gray_image, COLOR_BGR2GRAY);
+
+        vector<Rect> faces;
+        detect_face(gray_image, faces);
+        draw_faces(image, faces);
 
         imshow(window_name, image);
         
